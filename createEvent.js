f84272ecde48a11cb3b1294e6786f6c2f7835673
mamadont <m.coulibaly23@gmail.com>
2020-05-03 20:04:08 -0400

    Update createEvent.js

--- createEvent.js
+++ createEvent.js
@@ -13,29 +13,79 @@ class CreateEvent extends React.Component {
           EventTags:''
         };
       
-      // Handler for submission and testing purposes 
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+        this.onChangeEventName = this.onChangeEventName.bind(this);
+        this.onChangeEventDate = this.onChangeEventDate.bind(this);
+        this.onChangeEventTimeStart = this.onChangeEventTimeStart.bind(this);
+        this.onChangeEventTimeEnd = this.onChangeEventTimeEnd.bind(this);
+        this.onChangeEventLocation = this.onChangeEventLocation.bind(this);
+        this.onChangeEventDescription = this.onChangeEventDescription.bind(this);
+        this.onChangeEventHosts = this.onChangeEventHosts.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
     
 
-    // Sets value of input field upon change 
-    handleChange(event) {
-      this.setState({
-        EventNameValue: '',
-          EventDescription:'',
-          EventLocation:'',
-          EventDataAndTime:'',
-          Hosts:'',
-          EventTags:''
-      });
+    onChangeEventName(e) {
+        this.setState({
+            event_name: e.target.value
+        });
     }
-    
-    // Submission verification
-    handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.value);
-      event.preventDefault();
+
+    onChangeEventDate(e) {
+        var eventDate = formatDateYYYYmmDD(adjustTimeZone(e.target.value));
+        console.log(eventDate);
+        
+        this.setState({
+            event_date: eventDate
+        });
     }
+
+    onChangeEventTimeStart(e) {
+        this.setState({
+            event_timeStart: e.target.value,
+        });
+
+        console.log(e.target.value);
+    }
+
+    onChangeEventTimeEnd(e) {
+        this.setState({
+            event_timeEnd: e.target.value
+        });
+    }
+
+    onChangeEventLocation(e) {
+        this.setState({
+            event_location: e.target.value
+        });
+    }
+
+    onChangeEventDescription(e) {
+        this.setState({
+            event_description: e.target.value
+        });
+    }
+
+    onChangeEventHosts(e) {
+        this.setState({
+            event_hosts: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const newEvent = {
+            event_userId: this.state.event_userId, 
+            event_name: this.state.event_name,
+            event_date: this.state.event_date,
+            event_timeStart: this.state.event_timeStart,
+            event_timeEnd: this.state.event_timeEnd,
+            event_location: this.state.event_location,
+            event_description: this.state.event_description,
+            event_hosts: this.state.event_hosts,
+            event_attending: [ this.state.event_userId ],
+            event_numAttending: 1,
+        }
   
     render() {
       return (
